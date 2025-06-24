979d64b1976a16ce8a7d577d0cd7d204907fa492
Ravi Tharun <tharunravi672@gmail.com>
2025-06-24 17:42:13 +0530

    Fix: rename calendar.jsx to Calendar.jsx for Vercel build hi

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Components/Home.jsx";
 import Login from "./Components/Auth/Login.jsx";
-import Calendar from "./Components/Calendar.jsx"; // ✅ fixed
+import Calendar from "./Components/Calendar.jsx";
 import Signup from "./Components/Auth/sigup.jsx";
 import Team from "./Components/Team.jsx";
 import Acceptinvite from "./Components/Acceptinvite.jsx";
